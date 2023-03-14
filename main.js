149aae14faf3e71acab27fd4ba33194b0feff9be
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-03-14 18:27:36 +0530

    logo path change

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ import './style.css'
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "/assets/images/logo/logo.svg";
+      logo.src = "/assets/images/favicon.png";
     } else {
-      logo.src = "/assets/images/logo/logo.svg";
+      logo.src = "/assets/images/favicon.png";
     }
 
     // show or hide the back-top-top button
