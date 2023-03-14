7850d77f95a2cc98bb9d9e24e6cc6c156ed616af
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-03-14 19:32:53 +0530

    logoe

--- main.js
+++ main.js
@@ -18,9 +18,9 @@ import './style.css'
 
     // === logo change
     if (ud_header.classList.contains("sticky")) {
-      logo.src = "/assets/logo-fb267e3a.svg";
+      logo.src = "assets/logo.svg";
     } else {
-      logo.src = "/assets/logo-fb267e3a.svg";
+      logo.src = "assets/logo.svg";
     }
     // show or hide the back-top-top button
     const backToTop = document.querySelector(".back-to-top");
