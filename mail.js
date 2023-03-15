1782400ba7e9d2ce6207937e6562f929fab33d8d
Ayush Kumar <94730528+Akayush-17@users.noreply.github.com>
2023-03-15 12:08:05 +0530

    logo

--- mail.js
+++ mail.js
@@ -29,7 +29,7 @@ saveMessage(cname, emailid, phone, msg );
 
 document.querySelector('.alert').style.display="block";
 
-    setTimeout(()=>{
+setTimeout(()=>{
         document.querySelector('.alert').style.display="none";
 
     },2000);
